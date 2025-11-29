0fdbc803ba4a3a68f91d096ae4eec81408f1736f
Dominik Schullerer <89839452+DominikSchullerer@users.noreply.github.com>
2025-11-29 14:04:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function checkAnswer(day) {
+  const input = document.getElementById('answer').value.trim().toLowerCase();
+
+  // Lösungen pro Tag festlegen
+  const answers = {
+    1: "marzipan",
+    2: "weihnachten",
+    3: "kerze",
+  };
+
+  const successText = document.getElementById('success-text');
+
+  if (input === answers[day]) {
+    successText.classList.remove('hidden');
+  } else {
+    alert("Leider falsch, versuche es noch einmal!");
+  }
+}
